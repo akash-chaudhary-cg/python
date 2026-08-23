2601515b5e3e680038b9b9801d7971e1e757d98d
akash-chaudhary-cg <akash.chaudhary.cg@gmail.com>
2026-08-23 22:45:43 +0530

    introduction python

---
+++ 01-introduction-assignment/introduction.py
@@ -0,0 +1,9 @@
+#question 14
+#python is an high level programing languange
+a="Python in very user friendly"
+print(a)
+
+#question 15
+#it prints the word welcome
+a="welcome everyone how aare you all "
+print(a)
