ee801a9e3c2d26079c13ab53481a69c39405d510
akash-chaudhary-cg <akash.chaudhary.cg@gmail.com>
2026-08-23 22:39:32 +0530

    add variable assignment

---
+++ 02-variable-assignment/variable.py
@@ -0,0 +1,30 @@
+#Qustion-24
+name="Akash"
+age=19
+city="Tharad"
+print(name,age,city)
+#Question-25
+student_name = "Akash"
+student_roll=20
+student_branch="Btech cse"
+print(student_name,student_roll,student_branch)
+#Question-26
+marks=50
+marks=49
+print(marks)
+#Question-27
+name,age,city = "Akash",18,"Tharad"
+print(name,age,city)
+#Question-28
+x=y=z=0
+print(x,y,z)
+#Question-29
+student1 = "Rahul"
+student_name = "Rahul"
+student_branch= "B.Tech"
+print(student1,student_name,student_branch)
+#Question-30
+student_name="Akash"
+student_age=18
+student_age=20
+print(student_name,student_age)
