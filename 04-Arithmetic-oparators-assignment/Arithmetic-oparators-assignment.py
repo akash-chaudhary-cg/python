9e6a7908d388564ce6c7812d5f0a757a645b355f
akash-chaudhary-cg <akash.chaudhary.cg@gmail.com>
2026-08-26 10:05:01 +0530

    oparator are ready

---
+++ 04-Arithmetic-oparators-assignment/Arithmetic-oparators-assignment.py
@@ -0,0 +1,102 @@
+# task 1
+from pdb import pm
+
+
+a=5
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+# task 2
+a=5
+b=4.5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+print(type(a+b))
+print(type(a-b))
+print(type(a*b))
+print(type(a/b))
+print(type(a//b))
+print(type(a%b))
+print(type(a**b))
+
+#task 3
+a=4
+b=1
+c=5
+print("total marks :", a+b+c)
+print("average marks :", (a+b+c)/3)
+ #task 4
+product=10
+quantity=10
+print("total price :", product*quantity)
+#task 5
+fname="Akash"
+lname="chaudhary"
+print(fname+" "+lname)
+
+#6
+
+f="patel"
+print(f*3)
+
+#7
+
+x="perer"
+z="ytyty"
+print(x+z)
+print(x-z)
+print(x*z)
+print(x/z)
+
+#8
+
+print(8-6)
+print(6 - (-5))
+print(-8 - 7)
+print(-7 - (-8))
+
+#9
+
+print(8//6)
+print(6 // (-5))
+print(-8 // 7)
+print(-7 // (-8))
+
+#10
+
+print(8%6)
+print(6 % (-5))
+print(-8 % 7)
+print(-7 % (-8))
+ # extra
+
+a=4
+b=6
+c="akash"
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+print(c*a)
+print((-2)**4)
+print(-2**4)
+print(2**6)
+print(a%b)
+print(4%2//2)
+print(True + False)
+print(10/0)
